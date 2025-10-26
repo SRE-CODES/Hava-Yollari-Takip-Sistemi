8d0fd35ee1223733b024d67d68178f712570da9e
Sudenur <144936970+SudenurKececi@users.noreply.github.com>
2025-10-26 14:43:51 +0300

    Refactor SQL table creation statements

--- TabloOlusturmaHYTS.sql
+++ TabloOlusturmaHYTS.sql
@@ -1,48 +1,46 @@
 CREATE TABLE Airlines(
-airline_id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-name VARCHAR(100) NOT NULL,
-country VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE Airplanes(
-airplane_id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-model VARCHAR(50) NOT NULL,
-capacity INT NOT NULL,
-airline_id INT FOREIGN KEY REFERENCES Airlines(airline_id)
+airline_id INT IDENTITY(1,1) PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+country VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Airports(
-airport_id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+airport_id INT IDENTITY(1,1) PRIMARY KEY,
 name VARCHAR(100) NOT NULL,
-city VARCHAR(100) NOT NULL,
-country VARCHAR(100) NOT NULL
+city VARCHAR(50) NOT NULL,
+country VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Airplanes(
+airplane_id INT IDENTITY(1,1) PRIMARY KEY,
+model VARCHAR(100),
+capacity INT,
+airline_id INT FOREIGN KEY REFERENCES Airlines(airline_id)
 );
 
 CREATE TABLE Flights(
-flight_id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-flight_number VARCHAR(100) NOT NULL,
---airplane_id INT NOT NULL,
---departure_airport_id INT NOT NULL,
---arrival_airport_id INT NOT NULL,
---departure_time TIME NOT NULL,
---arrival_time TIME NOT NULL,
+flight_id INT IDENTITY(1,1) PRIMARY KEY,
+flight_number VARCHAR(20),
+departure_time DATETIME,
+arrival_time DATETIME,
 airplane_id INT FOREIGN KEY  REFERENCES Airplanes(airplane_id),
 departure_airport_id INT FOREIGN KEY  REFERENCES Airports(airport_id),
 arrival_airport_id INT FOREIGN KEY REFERENCES Airports(airport_id)
 );
 
 CREATE TABLE Passengers(
-passenger_id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+passenger_id INT IDENTITY(1,1) PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 surname VARCHAR(50) NOT NULL,
-passport_number INT NOT NULL UNIQUE
+passport_number VARCHAR(20) UNIQUE
 );
 
 CREATE TABLE Tickets(
-ticket_id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-seat_number INT NOT NULL,
-price INT NOT NULL,
+ticket_id INT IDENTITY(1,1) PRIMARY KEY,
+seat_number VARCHAR(10),
+price DECIMAL(10,2) NOT NULL,
 flight_id INT FOREIGN KEY  REFERENCES Flights(flight_id),
 passenger_id INT FOREIGN KEY  REFERENCES Passengers(passenger_id)
 );
+
 
